f7efa141d769f1a320fa467cd3576d10e7d1f1b2
Akbar <akbaruddin@live.in>
2021-03-16 00:50:03 +0530

    webpack js-babel and css-sass 🎉

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles/index.scss";
+
+console.log("run from index.js");
+const obj = {
+  name: "webpack",
+  type: "module",
+};
+
+const obj2 = {...obj};
+console.log(obj);
+console.log(obj2);
+console.log("webpack config.js");
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const M = require("minimatch");
+
+module.exports = {
+  devtool: "source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sc|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
